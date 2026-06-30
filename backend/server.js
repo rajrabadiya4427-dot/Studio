3c4aec4a003e099071709bcf89e8090f485ca410
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-06-30 19:47:35 +0530

    Initial commit with editing

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const port = process.env.PORT || 8000;
 const __dirname = path.resolve();
 
 
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+  contentSecurityPolicy: false,
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -1,4 +1,4 @@
-import { lazy} from "react";
+import { lazy, Suspense } from "react";
 import Hero from "../components/games/Hero";
 
 const GamesText = lazy(() => import("../components/games/GamesText"));
@@ -9,12 +9,11 @@ const Games = () => {
   return (
     <div>
       <Hero />
-
-      
+      <Suspense fallback={<div className="products-loading"><div className="animate-pulse flex space-x-4"><div className="flex-1 space-y-6 py-1"><div className="h-2 bg-slate-700 rounded"></div></div></div></div>}>
         <GamesText />
         <PublishSteps />
         <AllGames />
-     
+      </Suspense>
     </div>
   );
 };
