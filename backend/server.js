1a3ac5f767f940548f262bcec3ec9bad046b530e
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-06-30 19:34:52 +0530

    Initial commit with editing

--- backend/server.js
+++ backend/server.js
@@ -33,11 +33,11 @@ app.use("/api/comment", commentRouter);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.__dirname, "../frontend/dist"))
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
-  })
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 
